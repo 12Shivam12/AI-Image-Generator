d4ab5fa04f426eb59c03531ed9d65e87b72bea4f
12Shivam12 <120035361+12Shivam12@users.noreply.github.com>
2024-09-01 19:39:20 +0530

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
   const fetchPosts = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`http://localhost:8080/api/v1/post`, {
+      const response = await fetch(`https://ai-image-generator-3i3o.onrender.com/api/v1/post`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
